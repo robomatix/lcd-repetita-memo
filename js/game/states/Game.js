254c2c45fe12437cc079088acf6251c6a2145549
robomatix <robomatix@robomatix.net>
2015-01-04 07:53:12 +0100

    Generates 18 squares

--- js/game/states/Game.js
+++ js/game/states/Game.js
@@ -1,9 +1,11 @@
 repetita.Game = function () {
-// VARS
+//VARS
 };
 
 repetita.Game.prototype = {
     create: function () {
+
+        this.squaresCount=0;
 
         this.gameStartText = this.game.add.bitmapText(0, 0, 'mainFont', 'GAME !!!', 44);
         this.gameStartText.x = this.game.width / 2 - this.gameStartText.textWidth / 2;
@@ -14,43 +16,10 @@ repetita.Game.prototype = {
 
         this.simon = this.game.add.group();
 
-        /*
-        var i = 0;
-        while ( i < 18 ) {
-
-            var x, y, squareScale, square;
-
-            x = this.game.rnd.integerInRange(50, this.game.world.width - 50);
-            y = this.game.rnd.integerInRange(50, this.game.world.height - 50);
-            squareScale = this.game.rnd.integerInRange( 1, 4 );
-
-
-            square = new Square(this.game, x, y);
-
-            square.scale.setTo(squareScale);
-
-            this.game.physics.enable([square], Phaser.Physics.ARCADE);
-
-
-            if (!this.game.physics.arcade.overlap(square, this.simon)) {
-
-                this.simon.add(square);
-                console.log('NO overlap');
-                i++;
-
-            } else {
-
-                console.log('OOOOOverlap');
-                square.tint=0xff0000;
-                square.destroyThis();
-
-            }
-
-        }*/
 
         /* add a timer to generate squares
          ******************************************************/
-        this.squaresGenerator = this.game.time.events.repeat(250,18, this.generateSquares, this);
+        this.squaresGenerator = this.game.time.events.repeat(250,1000, this.generateSquares, this);
         this.squaresGenerator.timer.start();
 
 
@@ -89,6 +58,8 @@ repetita.Game.prototype = {
 
             this.simon.add(square);
             console.log('NO overlap');
+            this.squaresCount++;
+
 
 
         } else {
@@ -96,6 +67,13 @@ repetita.Game.prototype = {
             console.log('OOOOOverlap');
             square.tint=0xff0000;
             square.destroyThis();
+
+        }
+        console.log(this.squaresCount);
+
+        if(this.squaresCount === 18){// We want 18 squares
+
+            this.squaresGenerator.timer.stop();
 
         }
 
